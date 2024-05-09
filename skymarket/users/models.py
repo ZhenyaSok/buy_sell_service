3e16a9336d7b5dbebb3be66f79462bf0b09ac633
Jenny <kapitan.kub@gmail.com>
2024-05-09 16:34:39 +0500

    создана Модель пользователя (поля по тз)

--- skymarket/users/models.py
+++ skymarket/users/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from users.managers import UserManager
 from phonenumber_field.modelfields import PhoneNumberField
 from django.utils.translation import gettext_lazy as _
-
+NULLABLE = {"null": True, "blank": True}
 
 class UserRoles:
     # TODO закончите enum-класс для пользователя
@@ -13,4 +13,31 @@ class UserRoles:
 class User(AbstractBaseUser):
     # TODO переопределение пользователя.
     # TODO подробности также можно поискать в рекоммендациях к проекту
-    pass
+
+    """Модель пользователя"""
+
+    ROLES = (
+        ('user', 'User'),
+        ('admin', 'Admin'),
+    )
+
+    username = None
+    email = models.EmailField(unique=True, verbose_name='Email')
+    first_name = models.CharField(max_length=50, verbose_name='Имя', **NULLABLE)
+    last_name = models.CharField(max_length=75, verbose_name='Фамилия', **NULLABLE)
+    phone = models.CharField(max_length=35, verbose_name='номер телефона', **NULLABLE)
+    image = models.ImageField(upload_to='user/', verbose_name='Фото', **NULLABLE)
+    role = models.CharField(max_length=5, choices=ROLES, default='user', verbose_name='статус пользователя')
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.email} - {self.last_name}'
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
+
+
