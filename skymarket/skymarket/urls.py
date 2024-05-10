a9630d71bfb06c09a444bb4ef84dd03f0e75abba
Jenny <kapitan.kub@gmail.com>
2024-05-10 18:16:54 +0500

    Не значительные правки

--- skymarket/skymarket/urls.py
+++ skymarket/skymarket/urls.py
@@ -18,14 +18,16 @@ schema_view = get_schema_view(
    public=True,
    permission_classes=(permissions.AllowAny,),
 )
-# TODO здесь необходимо подклюючит нужные нам urls к проекту
+
 
 urlpatterns = [
     path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path("api/admin/", admin.site.urls),
     path("api/redoc-tasks/", include("redoc.urls")),
+
     path("api/", include("users.urls")),
+    path("api/", include("ads.urls", namespace="ads")),
 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
