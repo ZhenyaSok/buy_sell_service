f6c2a4e3ec2a2ebc0950a5914f05480ed808c236
Jenny <kapitan.kub@gmail.com>
2024-05-10 17:54:02 +0500

    Сериализаторы для двух моделей обьявления и комменты

--- skymarket/ads/serializers.py
+++ skymarket/ads/serializers.py
@@ -1,18 +1,35 @@
 from rest_framework import serializers
-
-
-# TODO Сериалайзеры. Предлагаем Вам такую структуру, однако вы вправе использовать свою
+from ads.models import Ad, Comment
 
 class CommentSerializer(serializers.ModelSerializer):
-    # TODO сериалайзер для модели
-    pass
+    author_first_name = serializers.CharField(source="author.first_name", read_only=True)
+    author_last_name = serializers.CharField(source="author.last_name", read_only=True)
+    author_avatar = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+    def get_author_avatar(self, obj):
+        request = self.context.get("request")
+        if obj.author.avatar:
+            return request.build_absolute_uri(obj.author.avatar.url)
 
 
 class AdSerializer(serializers.ModelSerializer):
-    # TODO сериалайзер для модели
-    pass
+    """Класс-сериализатор для модели Ad"""
+    class Meta:
+        model = Ad
+        fields = ('pk', 'title', 'price', 'description', 'image', 'created_at')
 
 
 class AdDetailSerializer(serializers.ModelSerializer):
-    # TODO сериалайзер для модели
-    pass
+    """Класс-сериализатор для просмотра детальной информации по объявлению"""
+    author_first_name = serializers.CharField(source="author.first_name", read_only=True)
+    author_last_name = serializers.CharField(source="author.last_name", read_only=True)
+    author_email = serializers.CharField(source="author.email", read_only=True)
+    author_phone = serializers.CharField(source="author.phone", read_only=True)
+
+    class Meta:
+        model = Ad
+        fields = '__all__'
