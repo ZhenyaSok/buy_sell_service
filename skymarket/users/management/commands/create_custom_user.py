8bbe2133c3eebe390ae2303b3b4ed1820c0d70a3
Jenny <kapitan.kub@gmail.com>
2024-05-10 19:20:35 +0500

    кастомная команда для создания пользователя

---
+++ skymarket/users/management/commands/create_custom_user.py
@@ -0,0 +1,14 @@
+from django.core.management import BaseCommand
+from ...models import User
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        user = User.objects.create(
+            email='zhenyapaiton@yandex.ru',
+            first_name='admin_main',
+            last_name='custom',
+            is_active=True,
+        )
+        user.set_password('123456')
+        user.save()
