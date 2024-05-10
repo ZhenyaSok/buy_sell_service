4c87dca1d04e6a934ceeba004cb963e6fe98ea05
Jenny <kapitan.kub@gmail.com>
2024-05-10 17:55:43 +0500

    Права доступа для приложения ADS

--- skymarket/ads/permissions.py
+++ skymarket/ads/permissions.py
@@ -1,1 +1,15 @@
-# TODO здесь производится настройка пермишенов для нашего проекта
+from rest_framework.permissions import BasePermission
+from users.models import UserRoles
+
+class IsAdminOrOwner(BasePermission):
+    message = "Вы не являетесь владельцем или администратором"
+
+    def has_permission(self, request, view):
+        """Проверка на аутентификацию"""
+        return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        """Проверка на владельца или админа"""
+        if request.user.role == UserRoles.ADMIN:
+            return True
+        return obj.author == request.user
