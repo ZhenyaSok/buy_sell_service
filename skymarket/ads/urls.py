4665c787c4cb1261154d3bcc5ba625c50913c1d0
Jenny <kapitan.kub@gmail.com>
2024-05-10 17:55:04 +0500

    URLS для приложения ADS

--- skymarket/ads/urls.py
+++ skymarket/ads/urls.py
@@ -1,8 +1,17 @@
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from ads.apps import SalesConfig
+from ads.views import AdViewSet, CommentViewSet
 
 # TODO настройка роутов для модели
 
+app_name = SalesConfig.name
 
+router = DefaultRouter()
+router.register(r'ads', AdViewSet, basename='ads')
+router.register(r'ads/(?P<ad_pk>\d+)/comments', CommentViewSet, basename='reviews')
+
+urlpatterns = []
 urlpatterns = [
-
+    path("", include(router.urls))
 ]
