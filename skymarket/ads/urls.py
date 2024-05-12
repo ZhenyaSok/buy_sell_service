86905b26bc54dfc16a738a7fb8e9dc283a510fe0
Jenny <kapitan.kub@gmail.com>
2024-05-12 20:40:31 +0500

    Роутер, настройка, боремся

--- skymarket/ads/urls.py
+++ skymarket/ads/urls.py
@@ -1,15 +1,18 @@
 from django.urls import include, path
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import SimpleRouter
 from .apps import SalesConfig
 from .views import AdViewSet, CommentViewSet
 
-# TODO настройка роутов для модели
+
 
 app_name = SalesConfig.name
 
-router = DefaultRouter()
+router = SimpleRouter()
 router.register(r'ads', AdViewSet, basename='ads')
-router.register(r'ads/(?P<ad_pk>\d+)/comments', CommentViewSet, basename='comment')
+router.register(r'comments', CommentViewSet, basename='comment')
+# router.register(r'ads/(?P<ad_pk>\d+)/comments', CommentViewSet, basename='comment')
+
+
 
 urlpatterns = [
     path("", include(router.urls))
