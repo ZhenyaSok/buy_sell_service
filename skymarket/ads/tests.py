cc01fbee6182b6c922e5ae0f39493f86514da68c
Jenny <kapitan.kub@gmail.com>
2024-05-14 22:21:35 +0500

    Начала писать тесты

--- skymarket/ads/tests.py
+++ skymarket/ads/tests.py
@@ -34,68 +34,38 @@ class AdViewSetsTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 
-    def test_ad_list(self):
-        """Тест на получение листа приятных привычек"""
+    def test_create_ad(self):
+        url = reverse('ads:ads-list')
+        data = {
+            'title': 'second ad',
+            'description': 'test2'
+        }
+        author = self.user
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        ad = Ad.objects.get(title='second ad')
+        self.assertEqual(author, ad.author)
 
-        response = self.client.get(reverse('ads:ads-list'))
+    def test_retrieve_ad(self):
+        url = reverse('ads-detail', args=[self.ad.pk])
+        response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-    #
-    # def test_ads_create(self):
-    #
-    #     """ Тест создания объявления """
-    #
-    #
-    #     data = {
-    #         "title": "1",
-    #         "price": 10,
-    #         "author": self.user.id,
-    #         "description": "rdgf"
-    #     }
-    #
-    #     response = self.client.post(
-    #         reverse('ads:ads-list'),
-    #         data=data
-    #     )
-    #
-    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-    #
-    #     self.assertTrue(
-    #         Ad.objects.all().exists()
-    #     )
-    #
-    #     self.assertEqual(
-    #         response.json()['title'],
-    #         data['title']
-    #     )
 
-        # self.assertEqual(
-        #     response.json()['price'],
-        #     data['price']
-        # )
-        #
-        # self.assertEqual(
-        #     response.json()['description'],
-        #     data['description']
-        # )
+    def test_update_ad(self):
+        url = reverse('ads-detail', args=[self.ad.pk])
+        data = {
+            'title': 'update second ad',
+            'description': 'test2'
+        }
+        response = self.client.put(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        val = Ad.objects.get(pk=self.ad.pk)
+        self.assertEqual(val, 'update second ad')
 
 
-    # def test_ads_update(self):
-    #     """ ad updating testing"""
-    #
-    #     self.client.force_authenticate(user=self.user)
-    #
-    #     self.ad = Ad.objects.create(
-    #         title="test",
-    #         price=10,
-    #         description="this is a creation test",
-    #         author=self.user
-    #     )
-    #
-    #     updated_data = {
-    #         'title': 'test updated',
-    #         'price': 15,
-    #         "description": "this is a updated test"
-    #     }
-    #
+
+
+
+
 
 
