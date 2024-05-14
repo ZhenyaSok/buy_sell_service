f071014ebfeeb61cd1f48ec881dcea9d9527e029
Jenny <kapitan.kub@gmail.com>
2024-05-14 19:06:47 +0500

    Настройки бд для докер

--- skymarket/skymarket/settings.py
+++ skymarket/skymarket/settings.py
@@ -122,28 +122,28 @@ DJOSER = {
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': os.getenv('DB_ENGINE'),
-#         'NAME': os.getenv('DB_NAME'),
-#         'USER': os.getenv('DB_USER'),
-#         'PASSWORD': os.getenv('DB_PASSWORD'),
-#         'HOST': os.getenv('DB_HOST'),
-#         'PORT': os.getenv('DB_PORT'),
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'buy_sell_service',
-            'USER': 'postgres',
-            'PASSWORD': '123456',
-            # 'HOST': 'db',
-            # 'PORT': '5432'
-        }
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
 }
+
+
+# DATABASES = {
+#     'default': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': 'skymarket',
+#             'USER': 'postgres',
+#             'PASSWORD': '123456',
+#             # 'HOST': 'db',
+#             # 'PORT': '5432'
+#         }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
