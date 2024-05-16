9652acd58970f14ded3d1594ff55a229be2cbe04
Jenny <kapitan.kub@gmail.com>
2024-05-16 22:32:07 +0500

    Настройки БД верные

--- skymarket/config/settings.py
+++ skymarket/config/settings.py
@@ -131,17 +131,17 @@ DJOSER = {
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
-
 
 # DATABASES = {
 #     'default': {
@@ -244,5 +244,13 @@ EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = os.getenv("EMAIL_PORT")
 
 
-
+# # Настройки почты, для отправки
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# # EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'zhenyapaiton@yandex.ru'
+# EMAIL_HOST_PASSWORD = 'yplghjyryjhqiwnd'
+# EMAIL_HOST = 'smtp.yandex.ru'
+# EMAIL_PORT = 465
+# EMAIL_USE_SSL = True
+# DEFAULT_FROM_EMAIL = 'zhenyapaiton@yandex.ru'
 
